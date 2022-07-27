e01ee4c49ee05adfac9740e39700e085ea898c98
oddbookworm <49015102+oddbookworm@users.noreply.github.com>
2022-07-26 20:19:17 -0500

    managed to handle both correct and incorrect credentials

--- cypress/Integration/PageObject/basic-auth.js
+++ cypress/Integration/PageObject/basic-auth.js
@@ -3,6 +3,11 @@ import HomePage from "./HomePage";
 
 class BasicAuthPage extends HomePage {
 
+    constructor() {
+		super();
+		this.success;
+	}
+
     // I tried to get this working with incorrect credentials,
     // but I was unsuccessful
     visitPage () {
@@ -21,6 +26,27 @@ class BasicAuthPage extends HomePage {
         return cy.contains("Congratulations! You must have the proper credentials.");
       }
 
+      visitPageModified (creds) {
+        this.visitHome();
+
+        let options = {
+			url: '/basic_auth/',
+			auth: creds,
+			failOnStatusCode: false
+		}
+
+        // cy.request(options).then((response) => {
+        //     cy.get('@success').as('success');
+		// 	if (response.isOkStatusCode) {
+		// 		cy.visit(options);
+		// 		// this.success = cy.contains("Congratulations!");
+        //         success = true;
+		// 	} else {
+		// 		success = false;
+		// 	}
+		// })
+        return cy.request(options);
+      }
 }
 
 export default BasicAuthPage
--- cypress/e2e/basic-auth.cy.js
+++ cypress/e2e/basic-auth.cy.js
@@ -2,13 +2,58 @@ import BasicAuthPage from "../Integration/PageObject/basic-auth";
 
 const page = new BasicAuthPage
 
-describe('Basic Auth page testing', () => {
+describe('Basic Auth page testing correct credentials', () => {
 
     // visit the page and login with the correct credentials
     it('Visit the page and input correct creds', () => {
-        let results = page.visitPage();
-        
-        results.should("exist");
+
+        let creds = {
+            username: 'admin',
+            password: 'admin'
+        }
+
+        let options = {
+            url: '/basic_auth/',
+            auth: creds
+        }
+
+        let request = page.visitPageModified(creds);
+        request.then((response) => {
+            if (response.isOkStatusCode) {
+                cy.visit(options);
+                let results = cy.contains('Congratulations');
+                results.should('exist');
+            } else {
+                let results = cy.contains('Examples');
+                results.should('exist');
+            }
+        })
+    })
+
+    // visit the page and login with incorrect credentials
+    it('Visit the page and input incorrect creds', () => {
+
+        let creds = {
+            username: 'user',
+            password: 'password'
+        }
+
+        let options = {
+            url: '/basic_auth/',
+            auth: creds
+        }
+
+        let request = page.visitPageModified(creds);
+        request.then((response) => {
+            if (response.isOkStatusCode) {
+                cy.visit(options);
+                let results = cy.contains('Congratulations');
+                results.should('exist');
+            } else {
+                let results = cy.contains('Examples');
+                results.should('exist');
+            }
+        })
     })
 
 })
