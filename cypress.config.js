bf812f2ac9a9b722ecc142e4915191164f928bce
oddbookworm <49015102+oddbookworm@users.noreply.github.com>
2022-07-28 21:46:50 -0500

    Tried to get allure working with the @shelex/cypress-allure-plugin plugin, but that didn't work

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://the-internet.herokuapp.com/'
-    // baseUrl: 'http://localhost:7080/'
+    baseUrl: 'https://the-internet.herokuapp.com/',
+    // baseUrl: 'http://localhost:7080/',
+
+    setupNodeEvents(on, config) {
+      AllureWriter(on, config);
+      return config;
+    }
   }
 });
